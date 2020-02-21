9aa4a211897a418cb6f2d1c057e1a4fb78b91372
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-02-21 11:32:56 -0500

    set up files to get started
    created actions/reducers folder, added a simple reducer, and updated app.js to create the store for redux

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
+import {createStore, applyMiddleware} from 'redux';
+import {reducer} from '../reducers/reducer';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import SmurfsList from './SmurfsList';
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+function App() {
+  return (
+    <div className="AppContainer">
+       <h1>Smurf Village</h1>
+       <SmurfsList/>
+    </div>
+  );
 }
 
 export default App;
---
+++ smurfs/src/components/SmurfsList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+const SmurfsList = (props) => {
+    return (
+        <div className="SmurfListContainer">
+            <h2>Smurf List Component</h2>
+        </div>
+    );
+}
+
+export default SmurfsList;
