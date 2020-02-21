9b476da89ca30f279817c5b8bb461bacf3657a68
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-02-21 15:30:57 -0500

    added comments about later changing the postData action
    this will allow for local storage of state of the entire object that gets sent back from the POST to the api, including the ID key of each new smurf. This isn't available to us under the created newSmurf object currently sent through to the local storage of state, as that id is created by the backend

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -26,7 +26,9 @@ export const postData = (newSmurf) => dispatch => {
     axios
         .post("http://localhost:3333/smurfs", newSmurf)
         .then (response => {
-            console.log(response)
+            console.log("postdata response", response) //change this to dispatch the entire response array, using UPDATE_CHARACTERS accordingly.
+            //this allows for you to get the 'ID' attribute back on each smurf in your local component storage, incase you want to use that for later. 
+            //The server creates this id automatically, and therefore isn't present in the "newSmurf" variable that was made
             dispatch({type: POST_DATA, payload: newSmurf})
         })
         .catch(error => {
